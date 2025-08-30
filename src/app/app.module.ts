4dd2ebb41e260129c12bc54bdae662b41f71c190
Michel Angelis araujo <michelangelisaraujo@MacBook-Pro-de-Michel.local>
2025-08-30 12:03:42 -0300

    chore: atualiza Angular de 17.3.12 para 18.2.13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ApiInterceptor } from './security/api.interceptor';
 import { ListDetailComponent } from './features/list-detail/list-detail.component';
 import { SharedModule } from './shared/shared.module';
@@ -13,23 +13,17 @@ import { registerLocaleData } from '@angular/common';
 
 registerLocaleData(localePt, "pt-br"); // currency: 'BRL':true 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ListDetailComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    SharedModule
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
-    { provide: LOCALE_ID, useValue: "pt-BR" },
-    { provide: DEFAULT_CURRENCY_CODE, useValue: "BRL" },
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+        ListDetailComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        SharedModule], providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
+        { provide: LOCALE_ID, useValue: "pt-BR" },
+        { provide: DEFAULT_CURRENCY_CODE, useValue: "BRL" },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule { }
