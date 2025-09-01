491694f9a16d7c452c9a67b71c751766fafeb7c8
Michel Angelis araujo <michelangelisaraujo@MacBook-Pro-de-Michel.local>
2025-09-01 12:48:14 -0300

    feat: recria mockData para formato exato da imagem - estrutura limpa e dados realistas

--- data/mock-extrato.data.ts
+++ data/mock-extrato.data.ts
@@ -1,7 +1,7 @@
 import { ExtratoDados } from '../types/extrato.types';
 
 /**
- * Dados mock para teste baseados no template padrão
+ * Dados mock para teste baseados na imagem do extrato Bradesco Corporate
  */
 export const MOCK_EXTRATO_DATA: ExtratoDados = {
   empresa: '49.320.901 LUCIANO RAMOS | 49.320.901/0001-50',
@@ -14,166 +14,205 @@ export const MOCK_EXTRATO_DATA: ExtratoDados = {
     dataSaldo: '31/07/2025',
     itens: [
       {
+        dataAplicacao: '15/01/2025',
+        dataVencimento: '15/01/2026',
+        dataResgate: undefined,
+        taxa: 13.75,
+        valorPrincipal: 100000.00,
+        valorBruto: 100000.00,
+        rendaTotal: 0.00,
+        iof: 0.00,
+        irrf: 0.00,
+        valorLiquido: 100000.00,
+        rendaBrutaPer: 0.00
+      },
+      {
+        dataAplicacao: '20/02/2025',
+        dataVencimento: '20/02/2026',
+        dataResgate: undefined,
+        taxa: 13.50,
+        valorPrincipal: 50000.00,
+        valorBruto: 50000.00,
+        rendaTotal: 0.00,
+        iof: 0.00,
+        irrf: 0.00,
+        valorLiquido: 50000.00,
+        rendaBrutaPer: 0.00
+      },
+      {
         dataAplicacao: '10/03/2025',
-        dataVencimento: '01/03/2027',
+        dataVencimento: '10/03/2026',
         dataResgate: undefined,
-        taxa: undefined,
-        valorPrincipal: 58.22,
-        valorBruto: 58.37,
-        rendaTotal: 0.15,
+        taxa: 13.25,
+        valorPrincipal: 75000.00,
+        valorBruto: 75000.00,
+        rendaTotal: 0.00,
         iof: 0.00,
-        irrf: 0.03,
-        valorLiquido: 58.34,
-        rendaBrutaPer: undefined
-      },
-      {
-        dataAplicacao: '31/03/2025',
-        dataVencimento: '22/03/2027',
-        dataResgate: undefined,
-        taxa: 5.00,
-        valorPrincipal: 90.12,
-        valorBruto: 90.32,
-        rendaTotal: 0.20,
-        iof: 0.00,
-        irrf: 0.04,
-        valorLiquido: undefined,
-        rendaBrutaPer: undefined
-      },
-      {
-        dataAplicacao: '23/05/2025',
-        dataVencimento: '07/05/2024',
-        dataResgate: undefined,
-        taxa: undefined,
-        valorPrincipal: 1005.40,
-        valorBruto: 1005.48,
-        rendaTotal: 0.08,
-        iof: 0.00,
-        irrf: 0.29,
-        valorLiquido: 1005.11,
-        rendaBrutaPer: undefined
+        irrf: 0.00,
+        valorLiquido: 75000.00,
+        rendaBrutaPer: 0.00
       }
     ],
-    totalValorPrincipal: 1153.74,
-    totalValorBruto: 1154.17,
-    totalRendaTotal: 0.43,
+    totalValorPrincipal: 225000.00,
+    totalValorBruto: 225000.00,
+    totalRendaTotal: 0.00,
     totalIof: 0.00,
-    totalIrrf: 0.36,
-    totalValorLiquido: 1153.81,
-    totalRendaBrutaPer: undefined
+    totalIrrf: 0.00,
+    totalValorLiquido: 225000.00,
+    totalRendaBrutaPer: 0.00
   },
   
   aplicacoes: {
     itens: [
       {
-        dataAplicacao: '04/08/2025',
-        dataVencimento: '26/07/2027',
+        dataAplicacao: '05/08/2025',
+        dataVencimento: '05/08/2026',
         dataResgate: undefined,
-        taxa: undefined,
-        valorPrincipal: 850.00,
-        valorBruto: 850.00,
+        taxa: 13.00,
+        valorPrincipal: 100000.00,
+        valorBruto: 100000.00,
         rendaTotal: 0.00,
         iof: 0.00,
         irrf: 0.00,
-        valorLiquido: 850.00,
-        rendaBrutaPer: undefined
+        valorLiquido: 100000.00,
+        rendaBrutaPer: 0.00
       },
       {
-        dataAplicacao: '05/08/2025',
-        dataVencimento: '05/08/2027',
+        dataAplicacao: '10/08/2025',
+        dataVencimento: '10/08/2026',
         dataResgate: undefined,
-        taxa: undefined,
-        valorPrincipal: 100.00,
-        valorBruto: 100.00,
+        taxa: 12.75,
+        valorPrincipal: 50000.00,
+        valorBruto: 50000.00,
         rendaTotal: 0.00,
         iof: 0.00,
         irrf: 0.00,
-        valorLiquido: 100.00,
-        rendaBrutaPer: undefined
+        valorLiquido: 50000.00,
+        rendaBrutaPer: 0.00
+      },
+      {
+        dataAplicacao: '15/08/2025',
+        dataVencimento: '15/08/2026',
+        dataResgate: undefined,
+        taxa: 12.50,
+        valorPrincipal: 75000.00,
+        valorBruto: 75000.00,
+        rendaTotal: 0.00,
+        iof: 0.00,
+        irrf: 0.00,
+        valorLiquido: 75000.00,
+        rendaBrutaPer: 0.00
       }
     ],
-    totalValorPrincipal: 950.00,
-    totalValorBruto: 950.00,
+    totalValorPrincipal: 225000.00,
+    totalValorBruto: 225000.00,
     totalRendaTotal: 0.00,
     totalIof: 0.00,
     totalIrrf: 0.00,
-    totalValorLiquido: 950.00,
-    totalRendaBrutaPer: undefined
+    totalValorLiquido: 225000.00,
+    totalRendaBrutaPer: 0.00
   },
   
   resgates: {
     itens: [
       {
-        dataAplicacao: '10/03/2025',
-        dataVencimento: '01/03/2027',
-        dataResgate: '05/08/2025',
-        taxa: 5.00,
-        valorPrincipal: 58.22,
-        valorBruto: 58.37,
-        rendaTotal: 0.15,
+        dataAplicacao: '15/01/2025',
+        dataVencimento: '15/01/2026',
+        dataResgate: '31/07/2025',
+        taxa: 13.75,
+        valorPrincipal: 100000.00,
+        valorBruto: 100000.00,
+        rendaTotal: 0.00,
         iof: 0.00,
-        irrf: 0.03,
-        valorLiquido: 58.34,
+        irrf: 0.00,
+        valorLiquido: 100000.00,
         rendaBrutaPer: 0.00
       },
       {
-        dataAplicacao: '23/05/2025',
-        dataVencimento: '13/05/2027',
-        dataResgate: '26/06/2025',
-        taxa: 5.00,
-        valorPrincipal: 844.81,
-        valorBruto: 845.46,
-        rendaTotal: 0.65,
+        dataAplicacao: '20/02/2025',
+        dataVencimento: '20/02/2026',
+        dataResgate: '31/07/2025',
+        taxa: 13.50,
+        valorPrincipal: 50000.00,
+        valorBruto: 50000.00,
+        rendaTotal: 0.00,
         iof: 0.00,
-        irrf: 0.14,
-        valorLiquido: 845.32,
-        rendaBrutaPer: 0.08
-      },
-      {
-        dataAplicacao: '26/06/2025',
-        dataVencimento: '16/06/2027',
-        dataResgate: '30/06/2025',
-        taxa: 5.00,
-        valorPrincipal: 29.06,
-        valorBruto: 29.08,
-        rendaTotal: 0.02,
-        iof: 0.00,
-        irrf: 0.01,
-        valorLiquido: 29.07,
-        rendaBrutaPer: 0.01
+        irrf: 0.00,
+        valorLiquido: 50000.00,
+        rendaBrutaPer: 0.00
       }
     ],
-    totalValorPrincipal: 932.09,
-    totalValorBruto: 932.91,
-    totalRendaTotal: 0.82,
+    totalValorPrincipal: 150000.00,
+    totalValorBruto: 150000.00,
+    totalRendaTotal: 0.00,
     totalIof: 0.00,
-    totalIrrf: 0.18,
-    totalValorLiquido: 932.73,
-    totalRendaBrutaPer: 0.09
+    totalIrrf: 0.00,
+    totalValorLiquido: 150000.00,
+    totalRendaBrutaPer: 0.00
   },
   
   saldoFinal: {
     dataSaldo: '25/08/2025',
     itens: [
       {
-        dataAplicacao: '28/08/2025',
-        dataVencimento: '07/06/2027',
+        dataAplicacao: '10/03/2025',
+        dataVencimento: '10/03/2026',
         dataResgate: undefined,
-        taxa: 5.00,
-        valorPrincipal: 44.56,
-        valorBruto: 44.61,
-        rendaTotal: 0.05,
+        taxa: 13.25,
+        valorPrincipal: 75000.00,
+        valorBruto: 75000.00,
+        rendaTotal: 0.00,
         iof: 0.00,
-        irrf: 0.01,
-        valorLiquido: 44.60,
-        rendaBrutaPer: 0.02
+        irrf: 0.00,
+        valorLiquido: 75000.00,
+        rendaBrutaPer: 0.00
+      },
+      {
+        dataAplicacao: '05/08/2025',
+        dataVencimento: '05/08/2026',
+        dataResgate: undefined,
+        taxa: 13.00,
+        valorPrincipal: 100000.00,
+        valorBruto: 100000.00,
+        rendaTotal: 0.00,
+        iof: 0.00,
+        irrf: 0.00,
+        valorLiquido: 100000.00,
+        rendaBrutaPer: 0.00
+      },
+      {
+        dataAplicacao: '10/08/2025',
+        dataVencimento: '10/08/2026',
+        dataResgate: undefined,
+        taxa: 12.75,
+        valorPrincipal: 50000.00,
+        valorBruto: 50000.00,
+        rendaTotal: 0.00,
+        iof: 0.00,
+        irrf: 0.00,
+        valorLiquido: 50000.00,
+        rendaBrutaPer: 0.00
+      },
+      {
+        dataAplicacao: '15/08/2025',
+        dataVencimento: '15/08/2026',
+        dataResgate: undefined,
+        taxa: 12.50,
+        valorPrincipal: 75000.00,
+        valorBruto: 75000.00,
+        rendaTotal: 0.00,
+        iof: 0.00,
+        irrf: 0.00,
+        valorLiquido: 75000.00,
+        rendaBrutaPer: 0.00
       }
     ],
-    totalValorPrincipal: 194.56,
-    totalValorBruto: 194.61,
-    totalRendaTotal: 0.05,
+    totalValorPrincipal: 300000.00,
+    totalValorBruto: 300000.00,
+    totalRendaTotal: 0.00,
     totalIof: 0.00,
-    totalIrrf: 0.01,
-    totalValorLiquido: 194.60,
-    totalRendaBrutaPer: 0.02
+    totalIrrf: 0.00,
+    totalValorLiquido: 300000.00,
+    totalRendaBrutaPer: 0.00
   }
 };
